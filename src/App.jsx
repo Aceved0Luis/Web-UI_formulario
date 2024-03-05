588414ad4063aefc0acce85fda1d5c2f46c26d72
Luis Acevedo <luisemilio.acevedo@globant.com>
2024-03-05 13:47:27 -0500

    funcionalidad casicompleta

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Formulario } from "./components/formulario"
 import { useEffect, useState } from "react";
 import { helpHttp } from './logic/fetchData.jsx'
-import { ticket } from './components/ticket.jsx';
+import { Ticket } from './components/ticket.jsx';
 function App() {
 
   const [db, setDb] = useState([])
+  const [inputTitle, setInputTitle] = useState("")
+  const [inputDescription, setInputDescription] = useState("")
+  const [selectPriority, setSelectPriority] = useState(1)
+  const [checkresul, setCheckResul] = useState(false)
+
   const api = helpHttp();
   const url = "http://localhost:3000/ticket";
 
@@ -14,24 +18,54 @@ function App() {
     });
     },[]);
 
-    const createData =(data) => {
-      let options ={
-          body:data,
-          headers: {"Content-Type": "application/json"}
+    const enviarDatos = (e) =>{
+      e.preventDefault();
+      const title = inputTitle;
+      const descripcion = inputDescription;
+      const priority = selectPriority;
+      const resuelto = checkresul;
+      if (title.length > 5 && title.length < 18){
+        fetch('http://localhost:3000/ticket', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title:title, description:descripcion, priority: priority, resolve:resuelto})
+        })
+          .then(res => {
+            return res.json()
+          })
+          .then(data => {
+            const nuevoArregloTickets = [...db, data];
+            setDb(nuevoArregloTickets);
+          })
       }
-      
-      api.post(url, options).then((response) => {
-        setDb(...db,response)
-      });
-  }
+    }
 
   return (
     <div className="container">
-      <Formulario action={createData} />
+      <h3>Agregar Ticket</h3>
+      <form className="formulario" onSubmit={enviarDatos}>
+            <label htmlFor="title">Titulo*</label>
+            <input value={inputTitle} onChange={(e) => setInputTitle(e.target.value)} placeholder="Titulo" type="text" id="title" name="title" required/>
+            <label htmlFor="priority">Prioridad*</label>
+            <select value={selectPriority} onChange={(e) => setSelectPriority(e.target.value)} className="priority" id="priority" name="priority">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+            <label htmlFor="descripcion">Descripción:</label>
+            <textarea value={inputDescription} onChange={(e) => setInputDescription(e.target.value)} placeholder="Descripcion" id="descripcion"></textarea>
+            <label>
+                <input value={checkresul} onChange={(e) => setCheckResul(e.target.checked)} type="checkbox"/>
+                Mark as resolve
+            </label>
+            <button type="submit" className="form-btn">Enviar</button>
+        </form>
       <h4>Current tickets</h4>
-      {db ? db.map((user)=> (
-        ticket(user.title, user.id, user.priority, user.description, user.resolve))): 
-        <h5>sin datos...</h5>}
+      {db.map((res)=>(
+        <Ticket titulo={res.title} id={res.id} prioridad={res.priority} descripcion={res.description} resuelto={res.resolve.toString()} />))
+      }
     </div>
   )
 }
--- src/components/ticket.jsx
+++ src/components/ticket.jsx
@@ -1,10 +1,10 @@
-export function ticket(titulo, id, prioridad, descripcion, resuelto) {
+export function Ticket({titulo, id, prioridad, descripcion, resuelto}) {
     return (
         <div className="tickets" key={id}>
             <h5>{titulo}</h5>
             <p>{descripcion}</p>
             <h6>Prioridad: {prioridad}</h6>
-            <h6>Resuelo: {resuelto.toString()}</h6>
+            <h6>Resuelo: {resuelto}</h6>
         </div>
     )
 }
